42bb894bee59c565d46658950a686a5ede0097b1
valakasgg <109357340+valakasgg@users.noreply.github.com>
2022-09-29 16:59:51 +0100

    basic but complete todo list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import { useState, useReducer } from 'react';
-import './App.css';
+import Todo from './Todo.js';
 
-const ACTIONS = {
-    ADD_TODO: 'add-todo'
+export const ACTIONS = {
+    ADD_TODO: 'add-todo',
+    TOGGLE_TODO: 'toggle-todo',
+    DELETE_TODO: 'delete-todo'
 }
 
 function reducer(todos, action) {
     switch(action.type) {
         case ACTIONS.ADD_TODO:
             return [...todos, newTodo(action.payload.name)]
+        case ACTIONS.TOGGLE_TODO:
+            return todos.map( todo => {
+                if(todo.id === action.payload.id) {
+                    return { ...todo, complete: !todo.complete }
+                }
+                return todo
+            })
+        case ACTIONS.DELETE_TODO:
+            return todos.filter(todo => todo.id !== action.payload.id)
+        default:
+            return todos
     }
 }
 
@@ -27,17 +40,17 @@ function App() {
         setName('')
     }
 
-    console.log(todos);
-
     return (
         <div className="form">
             <form onSubmit={handleSubmit}>
                 <input type="text" value={name} onChange={e => setName(e.target.value)} />
             </form>
-            <button>Go</button>
+            <button>Add</button>
             
             <div className="test">
-                {name}
+                {todos.map(todo => {
+                    return <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+                })}
             </div>
         </div>
     );
